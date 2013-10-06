dcf54e850e65bf2e2b9260967f70c14bf43e7af5
md_5 <md_5@live.com.au>
2013-10-06 17:36:28 +1100

    Don't Special Case X Move Value

--- src/main/java/net/minecraft/server/network/PlayerConnection.java
+++ src/main/java/net/minecraft/server/network/PlayerConnection.java
@@ -327,6 +327,7 @@ public class PlayerConnection implements ServerPlayerConnection, TickablePacketL
     private float lastPitch = Float.MAX_VALUE;
     private float lastYaw = Float.MAX_VALUE;
     private boolean justTeleported = false;
+    private boolean hasMoved; // Spigot
 
     public CraftPlayer getCraftPlayer() {
         return (this.player == null) ? null : (CraftPlayer) this.player.getBukkitEntity();
@@ -619,6 +620,7 @@ public class PlayerConnection implements ServerPlayerConnection, TickablePacketL
                     flag2 = true;
                     PlayerConnection.LOGGER.warn("{} (vehicle of {}) moved wrongly! {}", new Object[]{entity.getName().getString(), this.player.getName().getString(), Math.sqrt(d10)});
                 }
+                Location curPos = this.getCraftPlayer().getLocation(); // Spigot
 
                 entity.absMoveTo(d3, d4, d5, f, f1);
                 player.absMoveTo(d3, d4, d5, this.player.getYRot(), this.player.getXRot()); // CraftBukkit
@@ -633,6 +635,17 @@ public class PlayerConnection implements ServerPlayerConnection, TickablePacketL
 
                 // CraftBukkit start - fire PlayerMoveEvent
                 Player player = this.getCraftPlayer();
+                // Spigot Start
+                if ( !hasMoved )
+                {
+                    lastPosX = curPos.getX();
+                    lastPosY = curPos.getY();
+                    lastPosZ = curPos.getZ();
+                    lastYaw = curPos.getYaw();
+                    lastPitch = curPos.getPitch();
+                    hasMoved = true;
+                }
+                // Spigot End
                 Location from = new Location(player.getWorld(), lastPosX, lastPosY, lastPosZ, lastYaw, lastPitch); // Get the Players previous Event location.
                 Location to = player.getLocation().clone(); // Start off the To location as the Players current location.
 
@@ -658,7 +671,7 @@ public class PlayerConnection implements ServerPlayerConnection, TickablePacketL
                     this.lastPitch = to.getPitch();
 
                     // Skip the first time we do this
-                    if (from.getX() != Double.MAX_VALUE) {
+                    if (true) { // Spigot - don't skip any move events
                         Location oldTo = to.clone();
                         PlayerMoveEvent event = new PlayerMoveEvent(player, from, to);
                         this.cserver.getPluginManager().callEvent(event);
