b6903def635180bff696697ca72e36c36005b7df
Jonas Konrad <me@yawk.at>
2014-04-25 23:46:46 +0200

    Fix race condition that could kill connections before they were initiated
    Because NetworkManagers are registered before they get their channel in
    channelActive, the ServerConnection would remove them sometimes because
    it thought they were disconnected. This commit fixes this by introducing
    a 'preparing' variable that is true while the NetworkManager is not
    initialized. The ServerConnection does not remove NetworkManagers with
    this flag.

--- src/main/java/net/minecraft/network/NetworkManager.java
+++ src/main/java/net/minecraft/network/NetworkManager.java
@@ -77,6 +77,7 @@ public class NetworkManager extends SimpleChannelInboundHandler<Packet<?>> {
     // Spigot Start
     public java.util.UUID spoofedUUID;
     public com.mojang.authlib.properties.Property[] spoofedProfile;
+    public boolean preparing = true;
     // Spigot End
     private PacketListener packetListener;
     private IChatBaseComponent disconnectedReason;
@@ -98,6 +99,9 @@ public class NetworkManager extends SimpleChannelInboundHandler<Packet<?>> {
         super.channelActive(channelhandlercontext);
         this.channel = channelhandlercontext.channel();
         this.address = this.channel.remoteAddress();
+        // Spigot Start
+        this.preparing = false;
+        // Spigot End
 
         try {
             this.setProtocol(EnumProtocol.HANDSHAKING);
@@ -294,6 +298,9 @@ public class NetworkManager extends SimpleChannelInboundHandler<Packet<?>> {
     }
 
     public void disconnect(IChatBaseComponent ichatbasecomponent) {
+        // Spigot Start
+        this.preparing = false;
+        // Spigot End
         if (this.channel.isOpen()) {
             this.channel.close(); // We can't wait as this may be called from an event loop.
             this.disconnectedReason = ichatbasecomponent;
--- src/main/java/net/minecraft/server/network/ServerConnection.java
+++ src/main/java/net/minecraft/server/network/ServerConnection.java
@@ -186,6 +186,10 @@ public class ServerConnection {
                             networkmanager.setReadOnly();
                         }
                     } else {
+                        // Spigot Start
+                        // Fix a race condition where a NetworkManager could be unregistered just before connection.
+                        if (networkmanager.preparing) continue;
+                        // Spigot End
                         iterator.remove();
                         networkmanager.handleDisconnection();
                     }
