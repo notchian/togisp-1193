bd1dd7bd23bfd510ea42ecb726cf0da855923c8f
drXor <mcyoungsota@gmail.com>
2014-05-23 18:05:10 -0400

    Configurable save-on-stop-only for UserCache

--- src/main/java/net/minecraft/server/players/UserCache.java
+++ src/main/java/net/minecraft/server/players/UserCache.java
@@ -127,7 +127,7 @@ public class UserCache {
         UserCache.UserCacheEntry usercache_usercacheentry = new UserCache.UserCacheEntry(gameprofile, date);
 
         this.safeAdd(usercache_usercacheentry);
-        this.save();
+        if( !org.spigotmc.SpigotConfig.saveUserCacheOnStopOnly ) this.save(); // Spigot - skip saving if disabled
     }
 
     private long getNextOperation() {
@@ -159,7 +159,7 @@ public class UserCache {
             }
         }
 
-        if (flag) {
+        if (flag && !org.spigotmc.SpigotConfig.saveUserCacheOnStopOnly) { // Spigot - skip saving if disabled
             this.save();
         }
 
