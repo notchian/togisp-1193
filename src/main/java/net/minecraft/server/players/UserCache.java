pvginkel/Tweakkit-Server

Use Provided Case for Non Existent Offline Players

src/main/java/net/minecraft/server/UserCache.java
@@ -126,7 +126,7 @@ public class UserCache {
                 this.e.addFirst(gameprofile);
             }
         } else {
-            gameprofile = a(this.f, s1);
+            gameprofile = a(this.f, s); // Spigot - use correct case for offline players
             if (gameprofile != null) {
                 this.a(gameprofile);
                 usercacheentry = (UserCacheEntry) this.c.get(s1);

notchian/togisp-1193

src/main/java/net/minecraft/server/players/UserCache.java
@@ -152,7 +152,7 @@ public class UserCache {
             usercache_usercacheentry.setLastAccess(this.getNextOperation());
             optional = Optional.of(usercache_usercacheentry.getProfile());
         } else {
-            optional = lookupGameProfile(this.profileRepository, s1);
+            optional = lookupGameProfile(this.profileRepository, s); // Spigot - use correct case for offline players
             if (optional.isPresent()) {
                 this.add((GameProfile) optional.get());
                 flag = false;